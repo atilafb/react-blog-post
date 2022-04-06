35607fae5acadf64263d3c34200667168b6e1308
Atila <atilafb77@gmail.com>
2022-04-06 14:46:31 -0300

    feat: add sequential request logic

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,22 +1,57 @@
-import Axios from 'axios'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import api from '../services/api'
 import Post from './Post'
+
+const totalPosts = 95
+const pageSize = 20
+
+const arrayOfPages = (total, size) => {
+  const totalPages = total / size;
+  const fullPages = Math.floor(totalPages)
+  const partialPage = (totalPages - fullPages) * size
+
+  return [
+    ...new Array(fullPages).fill(size),
+    partialPage
+  ].filter(Boolean);
+}
+
+const fetchData = arrayOfPages(totalPosts, pageSize).map(
+  (maxPageLength, index) => () => {
+    console.log(`Calling ${index}`);
+    console.log(`index: ${index} and pageSize: ${pageSize}. Index times pageSize: ${index * pageSize}`)
+    return api.get(`/posts?`, {
+      params: {
+        _start: index * pageSize,
+        _limit: maxPageLength,
+      }
+    })
+    .then(res => res.data)
+    .catch((error) => {
+      throw new Error(`Failed loading posts on page ${index + 1}`)
+    })
+  }
+).reduce(
+  (chain, listPostFn) => chain.then((acc) => listPostFn().then((res) => [...acc, ...res])),
+  Promise.resolve([]));
 
 function PostList() {
   const [posts, setPosts] = useState([])
 
   const getPost = () => {
-    Axios.get("https://jsonplaceholder.typicode.com/posts?_start=0&_end=20").then((response) => {
-      console.log(response.data)
-      setPosts(response.data.map((post) =>
-        <Post key={post.id} post={post} />
-      ))
-    })
+    fetchData.then((response) => {
+      setPosts(response.map((post) => {
+        return <Post key={post.id} post={post} />
+      }))
+     })
   }
+
+  useEffect(() => {
+    getPost()
+  })
 
   return (
     <>
-      <button onClick={getPost}>Get Posts</button>
       {posts}
     </>
   );
