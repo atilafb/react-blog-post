7dac4d171b0c6073b4aeaaf68d2ea99bc316ab4b
Atila <atilafb77@gmail.com>
2022-04-08 17:53:57 -0300

    feat: render the first comment of each post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,8 +1,14 @@
 const Post = ({ post }) => {
+  
   return (
     <div>
       <h3>{post.title}</h3>
-      <p>{post.body}</p>
+      <h4>{post.body}</h4>
+      <ul>
+        <li>{post.comments[0].id}</li>
+        <li>{post.comments[0].name}</li>
+        <li>{post.comments[0].body}</li>
+      </ul>
     </div>
   );
 }
